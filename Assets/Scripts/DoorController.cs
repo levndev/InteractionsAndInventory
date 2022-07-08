bf03e4667f2217146c9f4b682fb3130a0b25f3c8
Lev Nazarov <levndev@gmail.com>
2022-07-08 20:41:58 +0500

    Двери можно закрывать на замок

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public HingeJoint2D Hinge;
+    public bool Locked;
+    private bool _locked;
+    JointAngleLimits2D UnlockedLimits;
+    private void Awake()
+    {
+        UnlockedLimits = Hinge.limits;
+    }
+
+    private void Update()
+    {
+        if (Locked != _locked)
+        {
+            _locked = Locked;
+            if (_locked)
+            {
+                Lock();
+            }
+            else
+            {
+                Unlock();
+            }
+        }
+    }
+
+    public void Lock()
+    {
+        Hinge.limits = new JointAngleLimits2D { max = 0, min = 0 };
+    }
+
+    public void Unlock()
+    {
+        Hinge.limits = UnlockedLimits;
+    }
+}
